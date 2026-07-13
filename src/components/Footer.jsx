9645baf02d81e2d592c92451de5054190b9ff0de
Mussussa <xismaelchaibo@gmail.com>
2026-07-13 12:18:36 +0200

    dando cor ao mapa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,10 @@ const Footer = () => {
   };
 
   return (
-    <footer className="w-full bg-slate-900 text-slate-300 py-10 md:py-16 mt-12 rounded-t-3xl border-t-4 border-blue-500" id="sobre">
+    <footer
+      className="w-full bg-slate-900 text-slate-300 py-10 md:py-16 mt-12 rounded-t-3xl border-t-4 border-blue-500"
+      id="sobre"
+    >
       <div className="max-w-[1600px] mx-auto px-6 lg:px-8">
         {/* Layout Grid: 1 Coluna (Mobile) -> 2 Colunas (Tablet) -> 4 Colunas (PC) */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-12">
@@ -202,19 +205,19 @@ const Footer = () => {
             <h3 className="text-white font-black text-lg uppercase tracking-wider border-b-2 border-blue-500 pb-2 inline-block text-center md:text-left">
               Encontre-nos
             </h3>
-<div className="w-full h-56 md:h-full min-h-[200px] bg-slate-800 rounded-2xl overflow-hidden shadow-inner relative border-2 border-slate-700/50">
-  <iframe
-    title="Mapa da Localização Grejudec"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.3769923954574!2d34.8824064!3d-19.823995399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1f2a41926b289b3f%3A0x43f98bc1102e9ff4!2sGrejudec!5e0!3m2!1spt-PT!2smz!4v1783937599328!5m2!1spt-PT!2smz"
-    width="100%"
-    height="100%"
-    style={{ border: 0 }}
-    allowFullScreen=""
-    loading="lazy"
-    referrerPolicy="strict-origin-when-cross-origin"
-    className="absolute inset-0 grayscale hover:grayscale-0 transition-all duration-500"
-  ></iframe>
-</div>
+            <div className="w-full h-56 md:h-full min-h-[200px] bg-slate-800 rounded-2xl overflow-hidden shadow-inner relative border-2 border-slate-700/50">
+              <iframe
+                title="Mapa da Localização Grejudec"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.3769923954574!2d34.8824064!3d-19.823995399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1f2a41926b289b3f%3A0x43f98bc1102e9ff4!2sGrejudec!5e0!3m2!1spt-PT!2smz!4v1783937599328!5m2!1spt-PT!2smz"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="strict-origin-when-cross-origin"
+                className="absolute inset-0 transition-all duration-500"
+              ></iframe>
+            </div>
           </div>
         </div>
 
